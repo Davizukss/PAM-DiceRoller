fbfc120a0682e4bb5b6bcffb5955676a49a19a53
Alitor004 <irmaosgomes1302@gmail.com>
2024-04-02 12:40:00 -0300

    diceroller

---
+++ DiceRoller/DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SorterNumeroDado_Clicked(object sender, EventArgs e)
+        {
+            var lados = EscolherLadoDado.SelectedItem;
+            if (lados != null)
+            {
+                var Nsorteado = new Random().Next(1, (int)lados + 1);
+                NumeroRodado.Text = Nsorteado.ToString();
+
+            }
+            else
+            {
+                NumeroRodado.Text = "Selecione um Valor";
+            }
+
+
+        }
+    }
+
+}
